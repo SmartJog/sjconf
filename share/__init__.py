77c9433b50ee504743079fd57f15b2bfe221b970
Philippe Bridant <philippe.bridant@smartjog.com>
2008-01-15 15:21:05 +0000

    base plugin code

--- share/__init__.py
+++ share/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-__all__ = ['test.py']
-version_info = (0,0,1)
-__version__ = "0.0.1"
-
+def init(base, local, conf):
+    pass
 
---
+++ share/snmpd.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+def init(base, local, conf):
+    pass
+
