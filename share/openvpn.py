9d8d757cdd59bce5f13aa4fd71d6ba3fddbee8f7
Philippe Bridant <philippe.bridant@smartjog.com>
2008-01-15 17:03:43 +0000

    first results

--- share/openvpn.py
+++ share/openvpn.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
-def init(sjconf, base, local, conf):
-    print open(sjconf["conf:base_path"] + "/" + conf["vpn:template"], "r").read() % conf
+from pprint import pprint
+
+def init(sjconf, base, local, config):
+    print open(sjconf["conf"]["base_path"] + "/" + config["vpn:template"], "r").read() % config
+
+    for i in config["network:inter_vpns"].split(","):
+        intervpn = dict(base["intervpn"])
+        intervpn.update(local[i])
+        print open(sjconf["conf"]["base_path"] + "/" + intervpn["template"], "r").read() % \
+            dict(map(lambda key: ("intervpn:" + key, intervpn[key]) , intervpn.keys()))
 
