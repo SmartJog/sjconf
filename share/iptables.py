811d62b8a1f10814723f79e5e984a795ff0c3bab
Philippe Bridant <philippe.bridant@smartjog.com>
2008-01-16 15:23:13 +0000

    working on it

--- share/iptables.py
+++ share/iptables.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 
+import os
+
 def init(sjconf, base, local, conf):
+    return []
+
+def save_confs():
     pass
 
+def apply_confs():
+    pass
+
+def rollback_confs():
+    pass
+
+def get_files_to_backup():
+    to_backup = []
+    return to_backup
+
+def restore_files(to_restore):
+    pass
+
+def restart_service(already_restarted):
+    if "iptables" not in already_restarted:
+        already_restarted += ["iptables"]
+        return os.system("/etc/init.d/ restart")
+    return 0
--- share/openvpn.py
+++ share/openvpn.py
@@ -2,12 +2,66 @@
 
 from pprint import pprint
 
+import os
+import iptables
+
+SERVICE_NAME='openvpn'
+OPENVPN_CONFDIR='/etc/openvpn/'
+
+INITD='/etc/init.d/openvpn'
+
 def init(sjconf, base, local, config):
-    print open(sjconf["conf"]["base_path"] + "/" + config["vpn:template"], "r").read() % config
+    new_confs = [{
+        'service'  : SERVICE_NAME,
+        'restart'  : INITD,
+        'filename' : '%s/%s.conf' % (OPENVPN_CONFDIR, local['rxtx']['hostname']),
+        'content'  : open(sjconf['conf']['base_path'] + '/' + config['vpn:template'], 'r').read() % config
+        }]
 
-    for i in config["network:inter_vpns"].split(","):
-        intervpn = dict(base["intervpn"])
+    for i in config['network:inter_vpns'].split(','):
+        intervpn = dict(base['intervpn'])
         intervpn.update(local[i])
-        print open(sjconf["conf"]["base_path"] + "/" + intervpn["template"], "r").read() % \
-            dict(map(lambda key: ("intervpn:" + key, intervpn[key]) , intervpn.keys()))
+        new_confs += [{
+            'service'  : SERVICE_NAME,
+            'restart'  : INITD,
+            'filename' : '%s/%s.conf' % (OPENVPN_CONFDIR, i), \
+            'content' : open(sjconf['conf']['base_path'] + '/' + intervpn['template'], 'r').read() % 
+                dict(map(lambda key: ('intervpn:' + key, intervpn[key]) , intervpn.keys()))}]
+    return new_confs
 
+def get_files_to_backup():
+    to_backup = []
+    for file in os.listdir(OPENVPN_CONFDIR):
+        if os.path.isfile(OPENVPN_CONFDIR + '/' + file):
+            if not (file.endswith('.crt') or file.endswith('.key')):
+                to_backup += [{'service' : SERVICE_NAME,
+                               'path'    : OPENVPN_CONFDIR + '/' + file}]
+    return to_backup
+
+def restore_files(to_restore):
+    for file in os.listdir(OPENVPN_CONFDIR):
+        path = os.path.isfile(OPENVPN_CONFDIR + '/' + file)
+        if os.path.isfile(path):
+            if not (file.endswith('.crt') or file.endswith('.key')):
+                os.unlink(path)
+
+    for file in list(to_restore):
+        if file['service'] == SERVICE_NAME:
+            os.rename(file['backup_path'], file['path'])
+            to_restore.remove(file)
+
+def save_confs():
+    pass
+
+def apply_confs():
+    pass
+
+def rollback_confs():
+    pass
+
+def restart_service(to_restart, already_restarted):
+    iptables.restart_service(already_restarted)
+    if SERVICE_NAME not in already_restarted:
+        already_restarted += [SERVICE_NAME]
+        return os.system('%s restart' % INITD)
+    return 0
