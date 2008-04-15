cf78070a1237a1ade2a38bcf2a084f258e86e3b6
Jean-Philippe Garcia Ballester <jeanphilippe.garciaballester@smartjog.com>
2008-04-15 13:29:24 +0000

    Add type 'bool'

--- sjconfparts/type.py
+++ sjconfparts/type.py
@@ -12,3 +12,19 @@ class Type:
     @classmethod
     def list_to_str(xcls, list_object):
         return ', '.join(list_object)
+
+    @classmethod
+    def str_to_bool(xcls, str_object):
+        if str_object == "yes" or str_object == "on" or str_object == "true":
+            return True
+        elif str_object == "no" or str_object == "off" or str_object == "false":
+            return False
+        else:
+            raise TypeError
+
+    @classmethod
+    def bool_to_str(xcls, bool_object):
+        if bool_object:
+            return "yes"
+        else:
+            return "no"
