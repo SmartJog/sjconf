6ee257d24378c296bd7e7a11d1518f8917683d2b
Jean-Philippe Garcia Ballester <jeanphilippe.garciaballester@smartjog.com>
2008-04-18 10:23:20 +0000

    Release version 1.1.0
    	* Add --install-distrib, --uninstall-distrib to install/uninstall a distrib configuration file.
    	* Add --enable-distrib, --disable-distrib to install/uninstall a distrib.
    	* Allow disabling an uninstalled plugin.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 setup(
         name = 'sjconf',
-        version = '1.1.0~dev',
+        version = '1.1.0',
         scripts = ['sjconf'],
         py_modules = ['sjconf'],
         packages = ['sjconfparts'],
