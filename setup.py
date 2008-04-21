6847e7d6ea8ff72853f52cbc8291985e52859e78
Jean-Philippe Garcia Ballester <jeanphilippe.garciaballester@smartjog.com>
2008-04-21 15:49:16 +0000

    Fix NameError exception, closes: #701
    	* sjconf: Use correct name for SJConfCLI.BadParam.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 setup(
         name = 'sjconf',
-        version = '1.1.0',
+        version = '1.1.1~dev',
         scripts = ['sjconf'],
         py_modules = ['sjconf'],
         packages = ['sjconfparts'],
