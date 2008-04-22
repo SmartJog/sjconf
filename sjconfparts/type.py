1c1d4a9bffcaa29f7a3f5826de4d965a251b73d8
Jean-Philippe Garcia Ballester <jeanphilippe.garciaballester@smartjog.com>
2008-04-22 10:51:16 +0000

    Better error message

--- sjconfparts/type.py
+++ sjconfparts/type.py
@@ -53,7 +53,7 @@ class Type(TypePythonIsCrappy):
 
         class BoolToStrError(TypePythonIsCrappy.ConversionError):
             def __init__(self, bool_object):
-                self.msg = 'Bad value "%s" for bool to str conversion, expected True or False' % (str_object)
+                self.msg = 'Bad value "%s" for bool to str conversion, expected a boolean' % (str_object)
 
         @classmethod
         def str_to_bool(xcls, str_object):
